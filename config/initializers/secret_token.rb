f2bdbe868d0344309b09c87cb38f5d403b45878c
平川翔一朗 <hirapy2201@gmail.com>
2016-05-10 17:30:25 +0900

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoveBoard::Application.config.secret_token = '6ccc46274f5a6605e1576aeee4b09d4e4edfd177857879e6e240f3d5a43efede272ee005868d05433c5b49483218d9830dab7c23513e86f9f1d31ec6ecb6f1bd'
